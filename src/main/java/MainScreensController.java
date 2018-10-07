af4ffe8b733ee5d21e53a7cf2da1de1cf0be541c
Ranieri <r176378@dac.unicamp.br>
2018-10-07 12:06:42 -0300

    Updated all methods in MainScreenController to use the hashmap.

--- src/main/java/MainScreensController.java
+++ src/main/java/MainScreensController.java
@@ -7,80 +7,67 @@ import java.io.IOException;
 import java.util.HashMap;
 
 class MainScreensController {
-        private static MainScreensController instance;
-        private int WIDTH = 1300;
-        private int HEIGHT = 700;
-        private static HashMap<String, Pane> screensMap = new HashMap<>();
-        private static Scene mainScene;
-        private static Parent root;
-        {
-            try {
-                root = FXMLLoader.load(getClass().getResource("/Screens/Menu/menu.fxml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private static MainScreensController instance;
+    private int WIDTH = 1300;
+    private int HEIGHT = 700;
+    private static HashMap<String, Pane> screensMap = new HashMap<>();
+    private static Scene mainScene;
+    private static Parent root;
+
+    {
+        try {
+            root = FXMLLoader.load(getClass().getResource("/Screens/Menu/menu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
     private MainScreensController() {
     }
 
-    static synchronized MainScreensController getInstance(){
-            if (instance == null){
-                instance = new MainScreensController();
-            }
-            return instance;
+    static synchronized MainScreensController getInstance() {
+        if (instance == null) {
+            instance = new MainScreensController();
+        }
+        return instance;
     }
 
     //Avoids rebiulding of already used screens, saving bult panes in a hashMap
-        private Pane getScreenFromMap(String fxmlPath){
-                Pane pane = screensMap.get(fxmlPath);
-            if (pane == null){
-                FXMLLoader loader = new FXMLLoader(MainScreensController.class.getResource(fxmlPath));
-                try {
-                    pane = loader.load();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                screensMap.put(fxmlPath, pane);
-            }
-            return pane;
-        }
-
-        Parent getRoot() {
-            return root;
-        }
-
-        Scene getMainScene() {
-            if (mainScene == null) {
-                mainScene = new Scene(root, WIDTH, HEIGHT);
-            }
-            return mainScene;
-        }
-
-        void showNewCenterScreen( String fxmlPath) {
-            ((BorderPane)root).setCenter(getScreenFromMap(fxmlPath));
-        }
-
-        void showNewTopScreen( String fxmlPath) {
-
+    private Pane getScreenFromMap(String fxmlPath) {
+        Pane pane = screensMap.get(fxmlPath);
+        if (pane == null) {
             FXMLLoader loader = new FXMLLoader(MainScreensController.class.getResource(fxmlPath));
-            Pane pane = null;
             try {
                 pane = loader.load();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            ((BorderPane)root).setTop(pane);
+            screensMap.put(fxmlPath, pane);
         }
-
-        void showNewFooterScreen( String fxmlPath) {
-            FXMLLoader loader = new FXMLLoader(MainScreensController.class.getResource(fxmlPath));
-            Pane pane = null;
-            try {
-                pane = loader.load();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            ((BorderPane)root).setBottom(pane);
-        }
+        return pane;
     }
+
+    Parent getRoot() {
+        return root;
+    }
+
+    Scene getMainScene() {
+        if (mainScene == null) {
+            mainScene = new Scene(root, WIDTH, HEIGHT);
+        }
+        return mainScene;
+    }
+
+    void showNewCenterScreen(String fxmlPath) {
+        ((BorderPane) root).setCenter(getScreenFromMap(fxmlPath));
+    }
+
+    void showNewTopScreen(String fxmlPath) {
+        ((BorderPane) root).setTop(getScreenFromMap(fxmlPath));
+    }
+
+    void showNewFooterScreen(String fxmlPath) {
+        ((BorderPane) root).setBottom(getScreenFromMap(fxmlPath));
+    }
+    
+}
