25127d3756c4c921115dc21a14425a9fef6d1a3e
Renan <renanlmg@gmail.com>
2018-11-26 20:26:59 -0200

    create model test

---
+++ src/main/java/Tests/ActionPlanTypeTest.java
@@ -0,0 +1,43 @@
+import Models.ActionPlanType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActionPlanTypeTest {
+
+    private ActionPlanType actionPlanType;
+
+    @Before
+    public void init() {
+        actionPlanType = new ActionPlanType();
+    }
+
+    @Test
+    public void getId() {
+        Assert.assertNull(actionPlanType.getId());
+    }
+
+    @Test
+    public void setId() {
+        actionPlanType.setId((Integer)234);
+        Assert.assertEquals((Integer)234, actionPlanType.getId());
+    }
+
+    @Test
+    public void getName() {
+        Assert.assertNull(actionPlanType.getName());
+    }
+
+    @Test
+    public void setName() {
+        actionPlanType.setName("TestName");
+        Assert.assertEquals("TestName",actionPlanType.getName());
+    }
+
+    @Test
+    public void equals() {
+        Assert.assertNotNull(actionPlanType);
+    }
+}
