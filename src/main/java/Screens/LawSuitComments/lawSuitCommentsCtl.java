8006dae16ef472d9b567ef3fd67861742f814535
Kauê Eufrosino Lima <k177650@g.unicamp.br>
2018-11-29 01:23:40 -0200

    Update lawSuitCommentsCtl.java

--- src/main/java/Screens/LawSuitComments/lawSuitCommentsCtl.java
+++ src/main/java/Screens/LawSuitComments/lawSuitCommentsCtl.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 
-public class LawSuitCommentsCtl extends Screen {
+public class lawSuitCommentsCtl extends Screen {
 
     @FXML
     DatePicker commentDatePicker;
